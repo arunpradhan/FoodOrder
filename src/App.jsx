49a92e86c23eed1c425cc794860a604f7450f0f0
arunpradhan <er.aru2010@gmail.com>
2026-01-20 08:20:41 +0530

    add routing in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Book from './pages/Book'
+import Menu from './pages/menu'
 
 function App() {
 
   return (
-    <>
-      FoodOrder
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>} />
+        <Route path='/menu' element={<Menu/>} />
+        <Route path='/book-order' element={<Book/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
