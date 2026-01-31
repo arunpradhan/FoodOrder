9ca8f5fe9d38fc5823a45677d195ab048736a1ae
arunpradhan <er.aru2010@gmail.com>
2026-01-31 18:32:03 +0530

    update book table or order page

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,3 +1,53 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 export default function Book() {
-    return <h1>Book Page</h1>;
+    return (
+        <section className="layout_padding">
+                <Container>
+                    <div className="heading_container">
+                        <h2>Book A Table</h2>
+                    </div>
+
+                    <Row>
+                        <Col xs={12} md={6}>
+                            <form>
+                                <div className="pt-3 pb-3">
+                                    <input type="text" className="form-control" placeholder="Enter Your Full Name" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <input type="text" className="form-control" placeholder="Enter Your Phone Number" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <input type="email" className="form-control" placeholder="Enter Your Email" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <select className="form-control">
+                                        <option>How Many Persons?</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <input type="date" className="form-control" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <button type="submit" className="button-link">
+                                        Book Now
+                                    </button>
+                                </div>
+                            </form>
+                        </Col>
+
+                        <Col xs={12} md={6}></Col>
+                    </Row>
+                </Container>
+            </section>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Col, Container, Row } from "react-bootstrap";
+import Book from "./Book";
 
 export default function Home() {
     return (
@@ -53,53 +54,7 @@ export default function Home() {
             </section>
 
             {/* BOOK A TABLE SECTION */}
-            <section className="layout_padding">
-                <Container>
-                    <div className="heading_container">
-                        <h2>Book A Table</h2>
-                    </div>
-
-                    <Row>
-                        <Col xs={12} md={6}>
-                            <form>
-                                <div className="pt-3 pb-3">
-                                    <input type="text" className="form-control" placeholder="Enter Your Full Name" />
-                                </div>
-
-                                <div className="pt-3 pb-3">
-                                    <input type="text" className="form-control" placeholder="Enter Your Phone Number" />
-                                </div>
-
-                                <div className="pt-3 pb-3">
-                                    <input type="email" className="form-control" placeholder="Enter Your Email" />
-                                </div>
-
-                                <div className="pt-3 pb-3">
-                                    <select className="form-control">
-                                        <option>How Many Persons?</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </select>
-                                </div>
-
-                                <div className="pt-3 pb-3">
-                                    <input type="date" className="form-control" />
-                                </div>
-
-                                <div className="pt-3 pb-3">
-                                    <button type="submit" className="button-link">
-                                        Book Now
-                                    </button>
-                                </div>
-                            </form>
-                        </Col>
-
-                        <Col xs={12} md={6}></Col>
-                    </Row>
-                </Container>
-            </section>
+            <Book/>
 
         </>
     );
