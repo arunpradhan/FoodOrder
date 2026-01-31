5ce98d4205fc29613b18c003176446c459285119
arunpradhan <er.aru2010@gmail.com>
2026-01-31 16:27:52 +0530

    Update Home Page design using - main and section semantics tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,82 +2,105 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export default function Home() {
     return (
-        <>
-            <Container-fluid>
-                <Row className="align-items-center">
-                    <Col>
-                        <img src="/images/hero-bg.jpg" className="w-100" alt="Header Image" />
-                    </Col>
-                </Row>
-            </Container-fluid>
-            <Container className="offer_section">
-                <Row className="align-items-center">
-                    <Col xs={12} md={6}>
-                        <div className="box">
-                            <div className="offer-img-box">
-                                <img src="images/o1.jpg" className="w-100" alt="" />
+        <main>
+
+            {/* HERO SECTION */}
+            <section className="hero-section">
+                <Container fluid className="hero-section-container">
+                    <Row className="align-items-center">
+                        <Col>
+                            <img
+                                src="/images/hero-bg.jpg"
+                                className="w-100"
+                                alt="Restaurant banner"
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            {/* OFFER SECTION */}
+            <section className="offer_section">
+                <Container>
+                    <Row className="align-items-center">
+                        <Col xs={12} md={6}>
+                            <div className="box">
+                                <div className="offer-img-box">
+                                    <img src="/images/o1.jpg" className="w-100" alt="Tasty Thursdays offer" />
+                                </div>
+                                <div className="offer-details-box">
+                                    <h5>Tasty Thursdays</h5>
+                                    <h6><span>20%</span> Off</h6>
+                                    <button className="button-link">Order Now</button>
+                                </div>
                             </div>
-                            <div className="offer-details-box">
-                                <h5>
-                                    Tasty Thursdays
-                                </h5>
-                                <h6>
-                                    <span>20%</span> Off
-                                </h6>
-                                <a href="" className="button-link">
-                                    Order Now
-                                </a>
+                        </Col>
+
+                        <Col xs={12} md={6}>
+                            <div className="box">
+                                <div className="offer-img-box">
+                                    <img src="/images/o2.jpg" className="w-100" alt="Pizza Days offer" />
+                                </div>
+                                <div className="offer-details-box">
+                                    <h5>Pizza Days</h5>
+                                    <h6><span>12%</span> Off</h6>
+                                    <button className="button-link">Order Now</button>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                    <Col xs={12} md={6}>
-                        <div className="box">
-                            <div className="offer-img-box">
-                                <img src="images/o2.jpg" className="w-100" alt="" />
-                            </div>
-                            <div className="offer-details-box">
-                                <h5>
-                                    Pizza Days
-                                </h5>
-                                <h6>
-                                    <span>12%</span> Off
-                                </h6>
-                                <a href="" className="button-link">
-                                    Order Now
-                                </a>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            <Container className="layout_padding">
-                <div class="heading_container">
-                    <h2>
-                        Book A Table
-                    </h2>
-                </div>
-                <Row>
-                    <Col xs={12} md={6}>
-                        <div className="">
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            {/* BOOK A TABLE SECTION */}
+            <section className="layout_padding">
+                <Container>
+                    <div className="heading_container">
+                        <h2>Book A Table</h2>
+                    </div>
+
+                    <Row>
+                        <Col xs={12} md={6}>
                             <form>
-                                <div className="pt-3 pb-3"><input type="text" name="name" className="form-control" placeholder="Enter Your Full Name" /></div>
-                                <div className="pt-3 pb-3"><input type="text" name="phonenumber" className="form-control" placeholder="Enter Your Phone Number" /></div>
-                                <div className="pt-3 pb-3"><input type="email" name="email" className="form-control" placeholder="Enter Your Email" /></div>
-                                <div className="pt-3 pb-3"><select className="form-control nice-select">
-                                    <option value="">How Many Persons?</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select></div>
-                                <div className="pt-3 pb-3"><input type="date" name="tabledate" className="form-control" placeholder="dd-mm-yyyy" /></div>
-                                <div className="pt-3 pb-3"><button type="button" className="button-link">Book Now</button></div>
+                                <div className="pt-3 pb-3">
+                                    <input type="text" className="form-control" placeholder="Enter Your Full Name" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <input type="text" className="form-control" placeholder="Enter Your Phone Number" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <input type="email" className="form-control" placeholder="Enter Your Email" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <select className="form-control">
+                                        <option>How Many Persons?</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <input type="date" className="form-control" />
+                                </div>
+
+                                <div className="pt-3 pb-3">
+                                    <button type="submit" className="button-link">
+                                        Book Now
+                                    </button>
+                                </div>
                             </form>
-                        </div>
-                    </Col>
-                    <Col xs={12} md={6}></Col>
-                </Row>
-            </Container>
-        </>
+                        </Col>
+
+                        <Col xs={12} md={6}></Col>
+                    </Row>
+                </Container>
+            </section>
+
+        </main>
     );
 }
